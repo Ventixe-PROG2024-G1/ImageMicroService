ba89554f7ecd4c98342342c56537f49e8ee04bd0
RamiObaid92 <163041066+RamiObaid92@users.noreply.github.com>
2025-05-17 01:39:59 +0200

    updates

---
+++ ImageServiceProvider/Functions/ImageResponseFunction.cs
@@ -0,0 +1,29 @@
+﻿using ImageServiceProvider.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace ImageServiceProvider.Functions;
+
+public class ImageResponseFunction(ILogger<ImageResponseFunction> logger, IAzureImageService azureImageService)
+{
+    private readonly ILogger<ImageResponseFunction> _logger = logger;
+    private readonly IAzureImageService _azureImageService = azureImageService;
+
+    [Function("ImageResponseFunction")]
+    public async Task<IActionResult> GetImage([HttpTrigger(AuthorizationLevel.Function, "get", Route = "images/{imageId:guid}")] HttpRequest req, Guid imageId)
+    {
+        _logger.LogInformation($"Starting image response process for image with ID: {imageId}");
+
+        var imageResponse = await _azureImageService.GetImageByIdAsync(imageId);
+        if (imageResponse == null)
+        {
+            _logger.LogWarning($"Image with ID: {imageId} not found");
+            return new BadRequestResult();
+        }
+
+        _logger.LogInformation($"Image with ID: {imageId} found, returning response");
+        return new OkObjectResult(imageResponse);
+    }
+}
---
+++ ImageServiceProvider/Functions/ImageUploadFunction.cs
@@ -0,0 +1,59 @@
+﻿using ImageServiceProvider.Models;
+using ImageServiceProvider.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using System.Net.Mime;
+
+
+namespace ImageServiceProvider.Functions;
+
+public class ImageUploadFunction(ILogger<ImageUploadFunction> logger, IAzureImageService azureImageService)
+{
+    private readonly ILogger<ImageUploadFunction> _logger = logger;
+    private readonly IAzureImageService _azureImageService = azureImageService;
+
+    [Function("ImageUploadFunction")]
+    public async Task<IActionResult> UploadImage([HttpTrigger(AuthorizationLevel.Function, "post", Route = "images")] HttpRequest req)
+    {
+        _logger.LogInformation("Starting image upload process");
+
+        if (!req.HasFormContentType)
+        {
+            _logger.LogWarning("Request does not contain form data");
+            return new BadRequestObjectResult("Request does not contain form data");
+        }
+
+        IFormFile? formFile = req.Form.Files.FirstOrDefault();
+
+        if (formFile == null || formFile.Length == 0)
+        {
+            _logger.LogWarning("No file found in the request");
+            return new BadRequestObjectResult("No file uploaded or file is empty");
+        }
+
+        string originalFileName = formFile.FileName;
+        string contentType = formFile.ContentType;
+        await using Stream fileStream = formFile.OpenReadStream();
+        _logger.LogInformation($"Attempting to upload file: {originalFileName}, Content Type: {contentType}");
+
+        ImageResponseModel? imageResponse = await _azureImageService.UploadFileAsync(fileStream, originalFileName, contentType);
+
+        if (imageResponse == null)
+        {
+            _logger.LogError("Failed to upload or save image");
+            return new BadRequestObjectResult("Failed to upload image or save image");
+        }
+
+        _logger.LogInformation($"Image uploaded successfully: {imageResponse.ImageId}");
+
+        return new CreatedAtActionResult(
+            "GetImage",
+            "ImageResponseFunction",
+            new { imageId = imageResponse.ImageId },
+            imageResponse);
+    }
+}
