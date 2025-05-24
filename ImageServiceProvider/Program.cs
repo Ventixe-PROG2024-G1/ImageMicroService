47bc08cf53aa48d3dbb9ac8ec910e34c61286f9e
RamiObaid92 <163041066+RamiObaid92@users.noreply.github.com>
2025-05-24 14:26:25 +0200

    BlobStorage connection fix

--- ImageServiceProvider/Program.cs
+++ ImageServiceProvider/Program.cs
@@ -1,4 +1,5 @@
-﻿using ImageServiceProvider.Data.Context;
+﻿using Azure.Storage.Blobs;
+using ImageServiceProvider.Data.Context;
 using ImageServiceProvider.Services;
 using ImageServiceProvider.Services.Handlers;
 using Microsoft.Azure.Functions.Worker;
@@ -20,5 +21,9 @@ builder.Services
 builder.Services.AddSingleton(typeof(ICacheHandler<>), typeof(CacheHandler<>));
 builder.Services.AddScoped<IAzureImageService, AzureImageService>();
 
+string blobConnectionString = Environment.GetEnvironmentVariable("BlobStorage:ConnectionString") ?? throw new InvalidOperationException("BlobStorage:ConnectionString is not set in the environment variables.");
+string blobContainerName = Environment.GetEnvironmentVariable("BlobStorage:ContainerName") ?? throw new InvalidOperationException("BlobStorage:ContainerName is not set in the environment variables.");
+
+builder.Services.AddSingleton(new BlobContainerClient(blobConnectionString, blobContainerName));
 
 builder.Build().Run();
