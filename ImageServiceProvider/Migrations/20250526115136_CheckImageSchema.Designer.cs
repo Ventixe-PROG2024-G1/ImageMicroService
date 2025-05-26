d1505eb4f5968dcbdbd87f1b21a99847e2fd84f6
RamiObaid92 <163041066+RamiObaid92@users.noreply.github.com>
2025-05-26 13:54:10 +0200

    Migration + Update database

--- ImageServiceProvider/Migrations/20250526115136_CheckImageSchema.Designer.cs
+++ ImageServiceProvider/Migrations/20250526115136_CheckImageSchema.Designer.cs
@@ -4,6 +4,7 @@ using ImageServiceProvider.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ImageServiceProvider.Migrations
 {
     [DbContext(typeof(ImageDbContext))]
-    partial class ImageDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250526115136_CheckImageSchema")]
+    partial class CheckImageSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
